72969a649180adc38910440e99fd1f9cfb5478f1
demo1 <amnijam60@gmail.com>
2025-11-21 01:02:24 +0300

    Fix Render root route + HEAD route

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
     import uvicorn
     import os
 
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     print("Starting server on port:", port)
 
     uvicorn.run("main:app", host="0.0.0.0", port=port)
